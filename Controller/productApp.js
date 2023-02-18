54440e5c82062efeaea3cce2ee26f2df1d30e00a
Anhkon-203 <you@example.com>
2023-02-18 16:18:03 +0700

    không có gì

--- Controller/productApp.js
+++ Controller/productApp.js
@@ -1,15 +1,48 @@
-const productApi =  "http://localhost:3000/products";
-app.controller('productController', function($scope, $http) {
-  $http.get(productApi)
-    .then(function(response) {
-      $scope.listProduct = response.data;
-    });
+const productApi = "http://localhost:3000/products";
+const categoryApi = "http://localhost:3000/categories";
 
-  $scope.product = {
-    name: "",
-    price: 0,
-    status: true,
-    category: 1,
-    image: ""
+app.controller('productController', function ($scope, $http) {
+  $http.get(categoryApi).then(function (response) {
+    $scope.listCategory = response.data;
+    // load products after categories are loaded
+    $http.get(productApi).then(function (response) {
+      $scope.listProduct = response.data.map(function (product) {
+        // replace category_id with category object
+        if (typeof product.category === 'string') {
+          var categoryId = parseInt(product.category);
+          if (!isNaN(categoryId)) {
+            product.category = $scope.listCategory.find(function (category) {
+              return category.id === categoryId;
+            });
+          }
+        }
+        return product;
+      });
+    });
+  });
+
+  $scope.filterByCategory = function(product) {
+    if (!$scope.selectedCategoryId) {
+      return true; // Nếu không chọn danh mục thì hiển thị tất cả sản phẩm
+    }
+    return product.category === $scope.selectedCategoryId;
+  };
+
+  $scope.getAllProducts = function() {
+    $http.get(productApi).then(function (response) {
+      $scope.listProduct = response.data.map(function (product) {
+        // replace category_id with category object
+        if (typeof product.category === 'string') {
+          var categoryId = parseInt(product.category);
+          if (!isNaN(categoryId)) {
+            product.category = $scope.listCategory.find(function (category) {
+              return category.id === categoryId;
+            });
+          }
+        }
+        return product;
+      });
+      $scope.selectedCategoryId = null; // Reset danh mục được chọn
+    });
   };
 });
