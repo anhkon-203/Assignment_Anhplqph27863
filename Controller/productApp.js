9eb3cb8a0b40e636929484b1c3f05aca6bd58a75
Anhkon-203 <you@example.com>
2023-02-19 22:21:00 +0700

    cart

--- Controller/productApp.js
+++ Controller/productApp.js
@@ -21,13 +21,41 @@ app.controller('productController', function($scope, $http) {
   };
 
   $scope.selectCategory = function(category) {
+    event.preventDefault();
     $scope.selectedCategory = category;
   };
 });
-app.controller('productDetailController', function($scope, $http, $routeParams) {
+app.controller('productDetailController', function($scope, $http, $routeParams,$rootScope,$location) {
+  event.preventDefault();
   var productId = $routeParams.productId;
   var id = $routeParams.id;
   $http.get(productApi + '/' + id).then(function(response) {
     $scope.product = response.data;
   });
+  $scope.addToCart = function(product, quantity) {
+    if (!$rootScope.currentUser) {
+      // Nếu người dùng chưa đăng nhập, yêu cầu người dùng đăng nhập
+      $location.path('/Index');
+      alert('Bạn phải đăng nhập để thực hiện chức năng này');
+    } else {
+      // Nếu người dùng đã đăng nhập
+      var cart = $rootScope.currentUser.cart;
+      if (!cart) {
+        // Nếu đối tượng giỏ hàng chưa tồn tại, tạo mới đối tượng giỏ hàng
+        cart = {
+          items: []
+        };
+        $rootScope.currentUser.cart = cart;
+      }
+      // Thêm sản phẩm vào đối tượng giỏ hàng
+      cart.items.push({
+        product: product,
+        quantity: quantity
+      });
+      // Lưu thông tin người dùng vào localStorage để giữ đăng nhập sau khi tắt trình duyệt
+      // localStorage.setItem('currentUser', JSON.stringify($rootScope.currentUser));
+      alert('Thêm sản phẩm vào giỏ hàng thành công');
+    }
+  };
+  
 });
